d67988ff3d71e9fb75591739315c7306312f0f52
Juan <jsandoval@rccl.com>
2023-02-13 04:17:39 -0600

    Add basic drag and drop functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,98 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { TaskItem } from './components/task-item/TaskItem'
-import { Task } from './models/task'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { TaskItem } from "./components/task-item/TaskItem";
+import { Task } from "./models/task";
 
-
-const storagedTasks = localStorage.getItem('tasks');
+const storagedTasks = localStorage.getItem("tasks");
 const initialTasks = storagedTasks ? JSON.parse(storagedTasks) : [];
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>(initialTasks);
-  const [taskName, setTaskName] = useState('');
+  const [taskName, setTaskName] = useState("");
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const updatedTasks = [...tasks, {name: taskName, id: new Date().getTime()}];
+    const updatedTasks = [
+      ...tasks,
+      { name: taskName, id: `${new Date().getTime()}` },
+    ];
     setTasks(updatedTasks);
-    setTaskName ('');
+    setTaskName("");
   };
 
   const handleRemove = (task: Task) => {
-    const updatedTasks = tasks.filter(item => item.id !== task.id);
+    const updatedTasks = tasks.filter((item) => item.id !== task.id);
     setTasks(updatedTasks);
-  }
+  };
 
   const toggleCompleted = (task: Task, completed: boolean) => {
-    const updatedTasks = tasks.map(taskItem => taskItem.id === task.id ? {...task, completed} : taskItem);
+    const updatedTasks = tasks.map((taskItem) =>
+      taskItem.id === task.id ? { ...task, completed } : taskItem
+    );
     setTasks(updatedTasks);
+  };
+
+  function dragstart_handler(ev: React.DragEvent<HTMLDivElement>) {
+    (ev.target as HTMLElement).style.opacity = '0.2';
+    (ev.target as HTMLElement).classList.add('grabbing');
+    ev.dataTransfer.effectAllowed = "copyMove";
+
+    //(ev.target as HTMLElement).style.cursor = 'grab';
+    ev.dataTransfer?.setData("text/plain", (ev.target as HTMLElement).id);
+    ev.dataTransfer.dropEffect = "copy";
   }
 
-  const clearAllTasks = () => {
-    setTasks([]);
+  function dragend_handler(ev: React.DragEvent<HTMLDivElement>) {
+    (ev.target as HTMLElement).style.opacity = '1';
+  }
+
+  function dragover_handler(ev: any) {
+    ev.preventDefault();
+  }
+
+  function drop_handler(ev: React.DragEvent<HTMLDivElement>) {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text/plain");
+    const draggedElem = document.getElementById(data) as HTMLElement;
+    (ev.target as HTMLElement).appendChild(draggedElem);
   }
 
   return (
     <div className="App">
+
+      <h3>Focus on:</h3>
+      <div className="TaskList" onDrop={drop_handler} onDragOver={dragover_handler}>
+        Drop tasks here
+      </div>
+
+      <h3>To Do Tasks </h3>
       <form onSubmit={handleFormSubmit}>
-        <input type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+        <input
+          type="text"
+          placeholder="New Task..."
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+          id="pichula"
+        />
       </form>
-      {
-        tasks.map(task =>
+      <div className="TaskList">
+        {tasks.map((task) => (
           <TaskItem
             key={task.id}
             task={task}
-            toggleComplete={(completed)=> {toggleCompleted(task, completed)}}
-            onRemove={() => handleRemove(task)} />
-        )
-      }
-      <br />
-      <button onClick={clearAllTasks}>Clear All</button>
+            toggleComplete={(completed) => toggleCompleted(task, completed)}
+            onRemove={() => handleRemove(task)}
+            onDragStart={dragstart_handler}
+            onDragEnd={dragend_handler}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/task-item/TaskItem.tsx
+++ src/components/task-item/TaskItem.tsx
@@ -1,17 +1,35 @@
-import { Task } from "../../models/task"
-import './TaskItem.css';
+import { Task } from "../../models/task";
+import "./TaskItem.css";
 type Props = {
   task: Task;
   toggleComplete: (completed: boolean) => void;
   onRemove: () => void;
-}
-export const TaskItem = ({task, toggleComplete, onRemove}: Props) => (
-  <div className="TaskItem">
+  onDragStart: (ev: React.DragEvent<HTMLDivElement>) => void;
+  onDragEnd: (ev: React.DragEvent<HTMLDivElement>) => void;
+};
+export const TaskItem = ({
+  task,
+  toggleComplete,
+  onRemove,
+  onDragStart,
+  onDragEnd,
+}: Props) => (
+  <div
+    className="TaskItem"
+    draggable="true"
+    onDragStart={onDragStart}
+    onDragEnd={onDragEnd}
+    id={task.id}
+  >
     <label>
-      <input type="checkbox" checked={task.completed} onChange={(e) => toggleComplete(e.target.checked)} />
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={(e) => toggleComplete(e.target.checked)}
+      />
+
       {task.name}
     </label>
     <button onClick={onRemove}>X</button>
   </div>
-
-)
+);
